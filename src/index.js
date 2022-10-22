b743b06f7c3ca41b9cd0fb7ecb1dd0aab00bff37
Orie Steele <orie@transmute.industries>
2022-10-22 10:24:26 -0500

    :rocket:

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const DID = require('./DID');
 const JWK = require('./JWK');
 const JWS = require('./JWS');
 
+const testVectors = require('./test-vectors.json');
+
 module.exports = {
   DID,
   JWK,
   JWS,
+  testVectors,
 };
